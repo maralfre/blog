e1394e80734b01faa59163972b777a9451503c29
maralfre <leoanlfredov@gmail.com>
2021-09-08 19:36:51 -0300

    simplificando codigos en comments_cotroller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,23 @@
 class CommentsController < ApplicationController
-    def create
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
-        redirect_to article_path(@article)
-      end
+  before_action :find_article, only: [:create, :destroy]  
+    
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
 
-      def destroy
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.find(params[:id])
-        @comment.destroy
-        redirect_to article_path(@article)
-      end
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
      
-      private
-        def comment_params
-          params.require(:comment).permit(:commenter, :body)
-        end
-    end 
+  private
+    def find_article 
+      @article = Article.find(params[:article_id])
+    end
+        
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+  end 
