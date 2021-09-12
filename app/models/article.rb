9b06a6a175e7e76694e5361e0a643c2fb9da07e5
maralfre <leoanlfredov@gmail.com>
2021-09-12 20:13:46 -0300

    refactorizando vistas con render y prueba callback before save

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true,
     length: { minimum: 5 }
+    before_save :mayuscula
+
+    def mayuscula
+      self.title = self.title.capitalize
+    end
 end
