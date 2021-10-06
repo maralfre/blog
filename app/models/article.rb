94330ee7893ab72ed99a2becaad4528e46edf78f
maralfre <leoanlfredov@gmail.com>
2021-10-06 18:00:16 -0300

    agregando moni test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ApplicationRecord
-    has_one_attached :photo
-    has_many :comments, dependent: :destroy
-    validates :title, presence: true,
-    length: { minimum: 5 }
-    before_save :mayuscula
+  has_one_attached :photo
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+  length: { minimum: 5 }
+  validates :text, presence: true
+  before_save :mayuscula
 
     def mayuscula
       self.title = self.title.capitalize
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = articles(:valid)
+  end
+  test 'valid article' do
+    assert @article.valid?
+  end
+
+  test 'invalid article without title' do
+    @article.title = nil
+    refute @article.valid?, 'article is valid without a title'
+    assert_not_nil @article.errors[:title], 'no validation error for title present'
+  end
+
+  test 'invalid article without text' do
+    @article.text = nil
+    refute @article.valid?
+    assert_not_nil @article.errors[:text]
+  end
 end
